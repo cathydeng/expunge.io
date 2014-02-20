1dce51923d57d1c37acdc147b7dda2109cf73b6e
Rene M. Paccha <paccha@gmail.com>
2014-02-20 10:46:04 -0600

    created lang-toggle button

--- app/controllers/set_language_controller.rb
+++ app/controllers/set_language_controller.rb
@@ -1,7 +1,22 @@
 class SetLanguageController < ApplicationController
+  
   def english
+    I18n.locale = :en
+    set_session_and_redirect
   end
 
   def spanish
+    I18n.locale = :es
+    set_session_and_redirect
   end
+
+  private
+  
+  def set_session_and_redirect
+    session[:locale] = I18n.locale
+    redirect_to :back
+  rescue ActionController::RedirectBackError
+    redirect_to :root
+  end
+  
 end
