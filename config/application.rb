b83cb1a46d6d44ee663bb764c95be7efe4a7a7f0
Rene M. Paccha <paccha@gmail.com>
2014-02-20 13:05:47 -0600

    making room for translations

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module Expungio
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'models','*', '*.yml').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'views','*', '*.yml').to_s]
+    config.i18n.default_locale = :en
   end
 end
