09a89948d8bf58bd5981035a184faac0ad57b8e5
Rene M. Paccha <paccha@gmail.com>
2014-03-14 23:54:17 -0500

    trying a mix in locales

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Expungio
     config.before_configuration do    
       I18n.load_path += Dir[Rails.root.join('config', 'locales', 'models','*', '*.yml').to_s]
       I18n.load_path += Dir[Rails.root.join('config', 'locales', 'views','*', '*.yml').to_s]
-      I18n.locale = :en
+      I18n.locale = :es
       I18n.default_locale = :en
       I18n.reload!
     end
