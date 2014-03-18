6bfe230faf50704e0064d2ebdd4a2ee6dc3b6fd3
Rene M. Paccha <paccha@gmail.com>
2014-03-18 13:45:39 -0500

    modified:   Gemfile to show updated sprockets gem 2.11, along with removing r18n gem

--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,15 @@ module Expungio
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    # config.before_configuration do
+    #   I18n.load_path += Dir[Rails.root.join('config', 'locales', 'views','*', '*.{rb,yml}').to_s]
+    #   I18n.locale = :en
+    #   I18n.default_locale = :en
+    #   I18n.reload!
+    # end
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'models','*', '*.yml').to_s]
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'views','*', '*.yml').to_s]
+    config.i18n.available_locales = [:es, :en]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'views', '*', '*.yml').to_s]
     config.i18n.default_locale = :en
   end
 end
