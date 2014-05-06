9ac22ea805a29743e33b1a298ad1ab929c37d8f1
Rene M. Paccha <paccha@gmail.com>
2014-05-05 22:34:13 -0500

    lang switch edits with modified:   config/routes.rb

--- app/controllers/set_language_controller.rb
+++ app/controllers/set_language_controller.rb
@@ -1,5 +1,5 @@
 class SetLanguageController < ApplicationController
-  
+
   def english
     I18n.locale = :en
     set_session_and_redirect
@@ -11,12 +11,11 @@ class SetLanguageController < ApplicationController
   end
 
   private
-  
+
   def set_session_and_redirect
     session[:locale] = I18n.locale
     redirect_to :back
   rescue ActionController::RedirectBackError
     redirect_to :root
   end
-  
 end
