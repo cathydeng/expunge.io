2cb70a82bcb6a7db2e72f7d75fc312bfd9f92680
Rene M. Paccha <paccha@gmail.com>
2014-05-06 14:35:52 -0500

    modified:   app/views/questions/index.html.erb to setup startpage translations, app/helpers/questions_helper.rb now has a helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,8 @@
 module QuestionsHelper
+
+  def medium_proceed_button text
+    href = "<button id='medium-text'>#{text}</button>"
+    link_to(href.html_safe, disclaimer_path, :method => "get")
+  end
+
 end
